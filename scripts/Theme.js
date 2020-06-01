7bfc3059773336a54ecf9ee0586c74e0b422f35c
Sohan Reddy <pingusohan@gmail.com>
2020-06-01 16:19:10 +0530

    First Commit

---
+++ scripts/Theme.js
@@ -0,0 +1,32 @@
+function Theme() {
+  if (document.getElementById('navbar').classList.contains('bg-light')) {
+    document.getElementById('navbar').classList.remove('bg-light');
+    document.getElementById('navbar').classList.remove('navbar-light');
+    Array.from(document.getElementsByClassName('text')).forEach((element) => {
+      element.classList.remove('text-dark');
+    });
+    document.getElementById('submit').style.backgroundColor = '#2980B9';
+    document.getElementById('navbar').classList.add('bg-dark');
+    document.getElementById('navbar').classList.add('navbar-dark');
+    Array.from(document.getElementsByClassName('text')).forEach((element) => {
+      element.classList.add('text-light');
+    });
+    document.getElementById('dark').innerHTML = 'Change Theme to Light';
+    document.body.style.backgroundColor = 'grey';
+  } else {
+    document.getElementById('navbar').classList.remove('bg-dark');
+    document.getElementById('navbar').classList.remove('navbar-dark');
+
+    Array.from(document.getElementsByClassName('text')).forEach((element) => {
+      element.classList.remove('text-light');
+    });
+    document.getElementById('submit').style.backgroundColor = 'green';
+    document.getElementById('navbar').classList.add('bg-light');
+    document.getElementById('navbar').classList.add('navbar-light');
+    Array.from(document.getElementsByClassName('text')).forEach((element) => {
+      element.classList.add('text-dark');
+    });
+    document.getElementById('dark').innerHTML = 'Change Theme to Dark';
+    document.body.style.backgroundColor = 'white';
+  }
+}
